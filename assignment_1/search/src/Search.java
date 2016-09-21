0e5c8278122cc25774029b5a920082edd2da077f
CFedderly <cfed182@hotmail.com>
2016-09-21 15:18:53 -0700

    Changed tabs to spaces in ProblemMap.java and Search.java. Fixes indent problems from last commit.

--- assignment_1/search/src/Search.java
+++ assignment_1/search/src/Search.java
@@ -79,7 +79,7 @@ public class Search {
     //
 
     private String TreeSearch(Frontier frontier) {
-		return TreeSearch(frontier, -1);
+        return TreeSearch(frontier, -1);
     }
 
     //None Depth Limited
@@ -89,109 +89,109 @@ public class Search {
     }
 
     //Depth Limited
-	private String GraphSearch(Frontier frontier, int limit) {
-		cnt = 0; 
-		node_list = new ArrayList<Node>();
-		
-		initialNode = MakeNode(problem.initialState); 
-		node_list.add( initialNode );
-		
-		Set<Object> explored = new HashSet<Object>(); //empty set
-		frontier.insert( initialNode );
-		while(true) {
-			
-			if(frontier.isEmpty())
-				return null;
-			
-			Node node = frontier.remove();
-			
-			if( problem.goal_test(node.state) )
-				return Solution(node);
-			
-			if( !explored.contains(node.state) && (node.depth < limit || limit < 0)) {
-				explored.add(node.state);
-				frontier.insertAll(Expand(node,problem));
-				cnt++;
-			}
-		}
-	}
-	
-	private String TreeSearch(Frontier frontier, int limit) {
-		cnt = 0;
-		node_list = new ArrayList<Node>();
+    private String GraphSearch(Frontier frontier, int limit) {
+        cnt = 0;
+        node_list = new ArrayList<Node>();
 
-		initialNode = MakeNode(problem.initialState);
-		node_list.add(initialNode);
+        initialNode = MakeNode(problem.initialState);
+        node_list.add( initialNode );
 
-		frontier.insert(initialNode);
-		while (true) {
+        Set<Object> explored = new HashSet<Object>(); //empty set
+        frontier.insert( initialNode );
+        while(true) {
 
-			if (frontier.isEmpty())
-				return null;
+            if(frontier.isEmpty())
+                return null;
 
-			Node node = frontier.remove();
+            Node node = frontier.remove();
 
-			if (problem.goal_test(node.state))
-				return Solution(node);
+            if( problem.goal_test(node.state) )
+                return Solution(node);
+
+            if( !explored.contains(node.state) && (node.depth < limit || limit < 0)) {
+                explored.add(node.state);
+                frontier.insertAll(Expand(node,problem));
+                cnt++;
+            }
+        }
+    }
+
+    private String TreeSearch(Frontier frontier, int limit) {
+        cnt = 0;
+        node_list = new ArrayList<Node>();
+
+        initialNode = MakeNode(problem.initialState);
+        node_list.add(initialNode);
+
+        frontier.insert(initialNode);
+        while (true) {
+
+            if (frontier.isEmpty())
+                return null;
+
+            Node node = frontier.remove();
+
+            if (problem.goal_test(node.state))
+                return Solution(node);
 
             if (limit < 0 || node.depth < limit ) {
                 frontier.insertAll(Expand(node, problem));
                 cnt++;
             }
-		}
-	}
+        }
+    }
 
-	private String GraphSearchDepthLimited(Frontier frontier, int limit) {
+    private String GraphSearchDepthLimited(Frontier frontier, int limit) {
         return GraphSearch(frontier, limit);
-	}
+    }
 
-	private String TreeSearchDepthLimited(Frontier frontier, int limit) {
-		return TreeSearch(frontier, limit);
-	}
+    private String TreeSearchDepthLimited(Frontier frontier, int limit) {
+        return TreeSearch(frontier, limit);
+    }
 
-	private Node MakeNode(Object state) {
-		Node node = new Node();
-		node.state = state;
-		node.parent_node = null;
-		node.path_cost = 0;
-		node.depth = 0;
-		return node;
-	}
-	
-	private Set<Node> Expand(Node node, Problem problem) {
-		node.order = cnt;
-		
-		Set<Node> successors = new HashSet<Node>(); //empty set
-		Set<Object> successor_states = problem.getSuccessors(node.state);
-		
-		for(Object result : successor_states) {
-			Node s = new Node();
-			s.state = result;
-			s.parent_node = node;
-			s.path_cost = node.path_cost + problem.step_cost(node.state, result); 
-			s.depth = node.depth + 1; 
-			successors.add(s);
-			
-			node_list.add( s );
-		}
-		
-		return successors;
-	}
-	
-	//Create a string to print solution. 
-	private String Solution(Node node) {
-		
-		String solution_str = "(cost=" + node.path_cost + ", expansions=" + cnt + ")\t";
-		
-		Deque<Object> solution = new ArrayDeque<Object>();
-		do {
-			solution.push(node.state);
-			node = node.parent_node;
-		} while(node != null);
-		
-		while(!solution.isEmpty())
-			solution_str += solution.pop() + " ";
-		
-		return solution_str;
-	}
+    private Node MakeNode(Object state) {
+        Node node = new Node();
+        node.state = state;
+        node.parent_node = null;
+        node.path_cost = 0;
+        node.depth = 0;
+        return node;
+    }
+
+    private Set<Node> Expand(Node node, Problem problem) {
+        node.order = cnt;
+
+        Set<Node> successors = new HashSet<Node>(); //empty set
+        Set<Object> successor_states = problem.getSuccessors(node.state);
+
+        for(Object result : successor_states) {
+            Node s = new Node();
+            s.state = result;
+            s.parent_node = node;
+            s.path_cost = node.path_cost + problem.step_cost(node.state, result);
+            s.depth = node.depth + 1;
+            successors.add(s);
+
+            node_list.add( s );
+        }
+
+        return successors;
+    }
+
+    //Create a string to print solution.
+    private String Solution(Node node) {
+
+        String solution_str = "(cost=" + node.path_cost + ", expansions=" + cnt + ")\t";
+
+        Deque<Object> solution = new ArrayDeque<Object>();
+        do {
+            solution.push(node.state);
+            node = node.parent_node;
+        } while(node != null);
+
+        while(!solution.isEmpty())
+            solution_str += solution.pop() + " ";
+
+        return solution_str;
+    }
 }
